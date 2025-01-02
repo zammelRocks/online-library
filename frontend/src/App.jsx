6ccb31156216fc0b08bb35bd0fe6872dc4db1c2b
Ons Zammel <onszammel@Onss-MacBook-Pro.local>
2025-01-02 17:17:12 +0100

    register Form component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import react from "react"
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
-import Login from "./pages/Login"
-import Register from "./pages/Register"
-import Home from "./pages/Home"
-import NotFound from "./pages/NotFound"
+import Login from "./pages/Login/"
+import Register from "./pages/Register/"
+import Home from "./pages/Home/"
+import NotFound from "./pages/NotFound/"
 import ProtectedRoute from "./components/ProtectedRoute"
 
 function Logout() {
@@ -23,9 +23,9 @@ function App() {
         <Route
           path="/"
           element={
-            <ProtectedRoute>
+            
               <Home />
-            </ProtectedRoute>
+           
           }
         />
         <Route path="/login" element={<Login />} />
